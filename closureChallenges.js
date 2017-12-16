4082041715f2b8ffbb6639583de5f5b577cdbf55
Michael Hueter <mthueter@gmail.com>
2017-12-16 15:49:08 -0800

    fixed test

--- closureChallenges.js
+++ closureChallenges.js
@@ -3,60 +3,30 @@
 over a 'count' variable that increments each time
 */
 
-function closureCounter() {
-  var count = 0;
-  return function() {
-    return ++count;
-  }
-}
+function closureCounter() {}
 
 // Tests for #1
 closureCounter()() // 1
 closureCounter()() // 1
 var firstCounter = closureCounter();
+firstCounter(); // 1
 firstCounter(); // 2
-firstCounter(); // 3
 var secondCounter = closureCounter();
-secondCounter(); // 2
-firstCounter(); // 4
-
-/* 
-1. Write a function 'counter' that returns a function with closure
-over a 'count' variable that increments each time
-*/
+secondCounter(); // 1
+firstCounter(); // 3
 
 /* 
 2. Write a function to act as a module (or API) using closure.
   It should have a private variable called "items" which is an empty array.
   It should return an object with four methods:
       addItem - insert an item at the end of the private array
-                and returns the inserted item
       removeItemAt - given an index, remove an item from the private array at that index,
                    and returns the removed item
       getItemAt - given an index, return an item from the private array with at that index
       getAll - return a copy of the private array
 */
 
-function itemModule() {
-  var privateArr = [];
-  return (function createModule() {
-    return {
-      addItem: function(item) {
-        privateArr.push(item);
-        return item;
-      },
-      getItemAt: function(idx) {
-        return privateArr[idx];
-      },
-      removeItemAt: function(idx) {
-        return privateArr.splice(idx, 1)[0];
-      },
-      getAll: function() {
-        return privateArr.concat();
-      }
-    }
-  })();
-}
+function itemModule() {}
 
 // Tests for #2
 var instance = itemModule();
